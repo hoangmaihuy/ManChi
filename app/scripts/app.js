d5ff43e431ac6de5f4f97c2653a4eb34ecbde1d6
Mai Huy Hoang <pynhp9x@gmail.com>
2016-09-29 22:59:41 +0800

    Rename project to MaChi

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('chineseApp', ['ui.router', 'ngResource'])
+angular.module('machiApp', ['ui.router', 'ngResource'])
   .config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('app', {
